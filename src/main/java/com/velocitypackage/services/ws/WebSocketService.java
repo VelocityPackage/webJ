18d97711b282b8d4e577b249b6e1db4d37c2c9db
MarvinMielchen <mielchen.2001@gmail.com>
2022-06-11 01:05:48 +0200

    Marvin

--- src/main/java/com/velocitypackage/services/ws/WebSocketService.java
+++ src/main/java/com/velocitypackage/services/ws/WebSocketService.java
@@ -92,49 +92,4 @@ public class WebSocketService
         }
         return result;
     }
-    
-    private byte[] encodeWebSocketMessage(String message){
-        byte[] rawData = message.getBytes();
-        int frameCount  = 0;
-        byte[] frame = new byte[10];
-        frame[0] = (byte) 129;
-        if(rawData.length <= 125){
-            frame[1] = (byte) rawData.length;
-            frameCount = 2;
-        }
-        if(rawData.length > 125 && rawData.length <= 65535){
-            frame[1] = (byte) 126;
-            int len = rawData.length;
-            frame[2] = (byte)((len >> 8 ) & (byte)255);
-            frame[3] = (byte)(len & (byte)255);
-            frameCount = 4;
-        }
-        if(rawData.length > 65535){
-            frame[1] = (byte) 127;
-            long len = rawData.length;
-            frame[2] = (byte)(0);
-            frame[3] = (byte)(0);
-            frame[4] = (byte)(0);
-            frame[5] = (byte)(0);
-            frame[6] = (byte)((len >> 24 ) & (byte)255);
-            frame[7] = (byte)((len >> 16 ) & (byte)255);
-            frame[8] = (byte)((len >> 8 ) & (byte)255);
-            frame[9] = (byte)(len & (byte)255);
-            frameCount = 10;
-        }
-        int bLength = frameCount + rawData.length;
-        byte[] reply = new byte[bLength];
-        int bLim = 0;
-        for(int i=0; i < frameCount; i++){
-            reply[bLim] = frame[i];
-            bLim++;
-        }
-        for (byte rawDatum : rawData)
-        {
-            reply[bLim] = rawDatum;
-            bLim++;
-        }
-        return reply;
-    }
-    
 }
