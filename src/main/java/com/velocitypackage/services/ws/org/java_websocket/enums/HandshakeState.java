73aee7ff017cc8aacc12b514958621b5915d75b8
MarvinMielchen <mielchen.2001@gmail.com>
2022-06-11 22:03:35 +0200

    marvin mielchen adds some features

---
+++ src/main/java/com/velocitypackage/services/ws/org/java_websocket/enums/HandshakeState.java
@@ -0,0 +1,15 @@
+package com.velocitypackage.services.ws.org.java_websocket.enums;
+
+/**
+ * Enum which represents the states a handshake may be in
+ */
+public enum HandshakeState {
+  /**
+   * Handshake matched this Draft successfully
+   */
+  MATCHED,
+  /**
+   * Handshake is does not match this Draft
+   */
+  NOT_MATCHED
+}
---
+++ src/main/java/com/velocitypackage/services/ws/org/java_websocket/enums/Role.java
@@ -0,0 +1,8 @@
+package com.velocitypackage.services.ws.org.java_websocket.enums;
+
+/**
+ * Enum which represents the states a websocket may be in
+ */
+public enum Role {
+  CLIENT, SERVER
+}
