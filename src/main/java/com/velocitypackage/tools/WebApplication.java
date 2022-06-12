4b47a3808dcfd4c74f5c47d703eca6dd3975888b
MarvinMielchen <mielchen.2001@gmail.com>
2022-06-12 13:37:13 +0200

    marvin mielchen adds some features

---
+++ src/main/java/com/velocitypackage/tools/WebApplication.java
@@ -0,0 +1,12 @@
+package com.velocitypackage.tools;
+
+import com.velocitypackage.materials.application.Website;
+
+public abstract class WebApplication
+{
+    public abstract Website build();
+    
+    public void start(){
+    
+    }
+}
--- src/test/java/com/velocitypackage/TestApp.java
+++ src/test/java/com/velocitypackage/TestApp.java
@@ -1,36 +1,18 @@
 package com.velocitypackage;
 
-import com.velocitypackage.materials.components.Button;
-import com.velocitypackage.materials.components.Page;
-import com.velocitypackage.tools.WebGraphicsApplication;
+import com.velocitypackage.materials.application.Website;
+import com.velocitypackage.tools.WebApplication;
 
-public class TestApp extends WebGraphicsApplication
+public class TestApp extends WebApplication
 {
+    public static void main(String[] args)
+    {
     
-    
-    public TestApp(){
-    
-        Page page = new Page();
-        setPage(page);
-        Button button = new Button("Button")
-        {
-            @Override
-            public void onClick()
-            {
-            
-            }
-        };
-        Button button2 = new Button("Button")
-        {
-            @Override
-            public void onClick()
-            {
-            
-            }
-        };
-        System.out.println(button.id);
-        System.out.println(button2.id);
-        
     }
     
+    @Override
+    public Website build()
+    {
+        return null;
+    }
 }
