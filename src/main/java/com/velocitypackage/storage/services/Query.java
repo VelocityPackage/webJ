b1f9d7fbdb6725599794bbd215e7c249c9d8f912
Waffelle <max@mielchen.email>
2022-06-20 17:49:07 +0200

    update

--- src/main/java/com/velocitypackage/storage/services/Query.java
+++ src/main/java/com/velocitypackage/storage/services/Query.java
@@ -1,13 +1,6 @@
 package com.velocitypackage.storage.services;
 
-import com.velocitypackage.storage.datatypes.SQLType;
-
-import java.util.Map;
-
 public class Query
 {
-    public static String createTable(String name, boolean primary, Map<String, SQLType> columns)
-    {
 
-    }
 }
