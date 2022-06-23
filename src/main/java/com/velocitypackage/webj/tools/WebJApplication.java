c289fc23cd438c4a22b476272de6939e3a7ceffc
Waffelle <max@mielchen.email>
2022-06-23 15:48:04 +0200

    update

--- src/main/java/com/velocitypackage/webj/tools/WebJApplication.java
+++ src/main/java/com/velocitypackage/webj/tools/WebJApplication.java
@@ -33,7 +33,6 @@ public abstract class WebJApplication
             {
                 try
                 {
-                    //unter den teppich kehren (Aka mike sagte mal man muss alles kommentieren wenn man codet)
                     return FileService.getContentOfResource("frame.html").replaceFirst("%NAME%", websiteTitle);
                 } catch (IOException e)
                 {
