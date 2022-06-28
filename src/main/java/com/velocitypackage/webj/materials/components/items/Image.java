2f11454af8a03314ec21bd2f9c13fbcd293ad1d4
Waffelle <max@mielchen.email>
2022-06-28 19:59:59 +0200

    update

--- src/main/java/com/velocitypackage/webj/materials/components/items/Image.java
+++ src/main/java/com/velocitypackage/webj/materials/components/items/Image.java
@@ -1,12 +1,11 @@
 package com.velocitypackage.webj.materials.components.items;
 
-import com.velocitypackage.webj.materials.hypertext.HyperTextElement;
-import com.velocitypackage.webj.materials.hypertext.Tag;
+import com.velocitypackage.webj.materials.hypertext.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Map;
 
@@ -14,20 +13,36 @@ public class Image extends Item
 {
     public Image (File image)
     {
-        setHyperTextElement(new HyperTextElement(Tag.IMG, null, null, null));
+        try
+        {
+            setHyperTextElement(new HyperTextElement(Tag.IMG, new Bootstrap[]{Bootstrap.IMG_FLUID}, new Attribute[]{new Attribute(Attribute.AttributeIdentifier.SRC, "data:image/webp;base64, " + toBase64(image))}, new Style[]{new Style(Style.StyleIdentifier.MAX_WIDTH, "100%"), new Style(Style.StyleIdentifier.HEIGHT, "auto")}));
+        } catch (IOException e)
+        {
+            setHyperTextElement(new HyperTextElement(""));
+        }
     }
     
     @Override
     public void onInteract(Map<String, String> values)
     {
-    
     }
     
     private String toBase64(File image) throws IOException
     {
-        FileInputStream fileInputStreamReader = new FileInputStream(image);
-        byte[] bytes = new byte[(int) image.length()];
-        fileInputStreamReader.read(bytes);
-        return new String(Base64.getDecoder().decode(Base64.getEncoder().withoutPadding().encodeToString(bytes)), StandardCharsets.UTF_8);
+        FileInputStream stream = new FileInputStream(image);
+        int bufLength = 2048;
+        byte[] buffer = new byte[2048];
+        byte[] data;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int readLength;
+        while ((readLength = stream.read(buffer, 0, bufLength)) != - 1)
+        {
+            out.write(buffer, 0, readLength);
+        }
+        data = out.toByteArray();
+        String imageString = Base64.getEncoder().withoutPadding().encodeToString(data);
+        out.close();
+        stream.close();
+        return imageString;
     }
 }
