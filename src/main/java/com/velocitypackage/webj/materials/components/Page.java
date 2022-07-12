092d494346461c748a300bce08bf383b21c577b7
Waffelle <max@mielchen.email>
2022-07-12 18:07:38 +0200

    update

--- src/main/java/com/velocitypackage/webj/materials/components/Page.java
+++ src/main/java/com/velocitypackage/webj/materials/components/Page.java
@@ -1,11 +1,10 @@
 package com.velocitypackage.webj.materials.components;
 
-import com.velocitypackage.webj.materials.hypertext.HyperTextBehavior;
 import com.velocitypackage.webj.materials.hypertext.HyperTextPage;
 
 public class Page extends HyperTextPage
 {
-    public Page(String path, HyperTextBehavior... children)
+    public Page(String path, Component... children)
     {
         super(path, children);
     }
