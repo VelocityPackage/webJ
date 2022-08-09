c003c788412382e2da01e01a4d5cc7d150d4c13a
Waffelle <max@mielchen.email>
2022-08-09 14:57:51 +0200

    update test tool

--- src/main/java/com/velocitypackage/webJ/tools/test/ApplicationTestTool.java
+++ src/main/java/com/velocitypackage/webJ/tools/test/ApplicationTestTool.java
@@ -1,6 +1,8 @@
 package com.velocitypackage.webJ.tools.test;
 
+import com.velocitypackage.webJ.materials.hypertext.Bootstrap;
 import com.velocitypackage.webJ.materials.hypertext.HyperTextPage;
+import com.velocitypackage.webJ.materials.hypertext.Style;
 import com.velocitypackage.webJ.materials.webJ.Application;
 import com.velocitypackage.webJ.services.http.HttpContext;
 
@@ -50,14 +52,34 @@ public class ApplicationTestTool
                                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
                                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
                                 </head>
-                                <body>
+                                <body style="%s" class="%s">
                                     %s
                                 </body>
                             </html>
-                            """, hyperTextPage.getTextRepresentation());
+                            """, styleInterpreter(hyperTextPage.getStyles()), bootstrapInterpreter(hyperTextPage.getBootstraps()), hyperTextPage.getTextRepresentation());
                 }
             });
         }
+    }
+    
+    private static String styleInterpreter(Style[] styles)
+    {
+        StringBuilder styleInterpretation = new StringBuilder();
+        for (Style style : styles)
+        {
+            styleInterpretation.append(style.getIdentifier().name().toLowerCase().replaceAll("_", "-")).append(":").append(style.getValue()).append(";");
+        }
+        return new String(styleInterpretation);
+    }
+    
+    private static String bootstrapInterpreter(Bootstrap[] bootstraps)
+    {
+        StringBuilder bootstrapInterpretation = new StringBuilder();
+        for (Bootstrap bootstrap : bootstraps)
+        {
+            bootstrapInterpretation.append(bootstrap.name().toLowerCase().replaceAll("_", "-")).append(" ");
+        }
+        return new String(bootstrapInterpretation);
     }
     
     /**
