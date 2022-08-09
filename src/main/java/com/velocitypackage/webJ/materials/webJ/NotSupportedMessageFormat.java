477165d2a70810b56a50d3eceea6ac12ce031760
Waffelle <max@mielchen.email>
2022-08-09 12:52:53 +0200

    NotSupportedMessageFormat exception add javadoc

--- src/main/java/com/velocitypackage/webJ/materials/webJ/NotSupportedMessageFormat.java
+++ src/main/java/com/velocitypackage/webJ/materials/webJ/NotSupportedMessageFormat.java
@@ -2,14 +2,25 @@ package com.velocitypackage.webJ.materials.webJ;
 
 import java.util.Arrays;
 
+/**
+ * Exception for unformatted messages or not supported formatted message
+ * @author maxmielchen
+ */
 @SuppressWarnings("unused")
 public class NotSupportedMessageFormat extends Exception
 {
+    /**
+     * Constructor
+     */
     public NotSupportedMessageFormat()
     {
         super();
     }
     
+    /**
+     * Constructor
+     * @param format supported format as array / varargs -> IMPORTANT: It's not a format check
+     */
     public NotSupportedMessageFormat(String... format)
     {
         super("Supported Formats: " + Arrays.toString(format));
