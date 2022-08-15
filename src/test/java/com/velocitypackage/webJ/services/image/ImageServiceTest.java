3e5063746cd8efc2f9b242bd947386ee69b8ec41
Waffelle <max@mielchen.email>
2022-08-15 12:08:02 +0200

    add ImageServiceTest

---
+++ src/test/java/com/velocitypackage/webJ/services/image/ImageServiceTest.java
@@ -0,0 +1,38 @@
+package com.velocitypackage.webJ.services.image;
+
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImageWriteException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageServiceTest
+{
+    File image = new File("src/test/resources/favicon.ico");
+    
+    @Test
+    void resizedIco() throws ImageWriteException, IOException, ImageReadException
+    {
+        System.out.println(Arrays.toString(ImageService.resizedIco(image, 1, 1)));
+        assertEquals("[0, 0, 1, 0, 1, 0, 1, 1, 2, 0, 1, 0, 1, 0, 56, 0, 0, 0, 22, 0, 0, 0, 40, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 67, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]", Arrays.toString(ImageService.resizedIco(image, 1, 1)));
+    }
+    
+    @Test
+    void icoToPng() throws IOException, ImageReadException
+    {
+        System.out.println(Arrays.toString(ImageService.icoToPng(image)));
+    }
+    
+    @Test
+    void resizedIcoToPng() throws IOException, ImageReadException
+    {
+        System.out.println(Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
+        assertEquals("[-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, -60, -119, 0, 0, 0, 13, 73, 68, 65, 84, 120, 94, 99, -48, 112, -74, -8, 3, 0, 2, -38, 1, -96, 53, -120, -25, 80, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126]", Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
+    }
+}
