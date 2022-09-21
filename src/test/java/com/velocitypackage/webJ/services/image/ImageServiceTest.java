4a99109e389c12e0afeb62d2af20eef00463cab8
Waffelle <max@mielchen.email>
2022-09-21 17:54:30 +0200

    import optimization & test fix

--- src/test/java/com/velocitypackage/webJ/services/image/ImageServiceTest.java
+++ src/test/java/com/velocitypackage/webJ/services/image/ImageServiceTest.java
@@ -2,15 +2,13 @@ package com.velocitypackage.webJ.services.image;
 
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.ImageWriteException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ImageServiceTest
 {
@@ -33,6 +31,11 @@ class ImageServiceTest
     void resizedIcoToPng() throws IOException, ImageReadException
     {
         System.out.println(Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
-        assertEquals("[-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, -60, -119, 0, 0, 0, 13, 73, 68, 65, 84, 120, 94, 99, -48, 112, -74, -8, 3, 0, 2, -38, 1, -96, 53, -120, -25, 80, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126]", Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
+        System.out.println(System.getProperty("java.version"));
+        if (!System.getProperty("java.version").startsWith("1.8"))
+        {
+            assertEquals("[-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, -60, -119, 0, 0, 0, 13, 73, 68, 65, 84, 120, 94, 99, -48, 112, -74, -8, 3, 0, 2, -38, 1, -96, 53, -120, -25, 80, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126]", Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
+        }
+        assertEquals("[-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, -60, -119, 0, 0, 0, 13, 73, 68, 65, 84, 120, -38, 99, -48, 112, -74, -8, 3, 0, 2, -38, 1, -96, 86, 73, -77, -49, 0, 0, 0, 0, 73, 69, 78, 68, -82, 66, 96, -126]", Arrays.toString(ImageService.resizedIcoToPng(image, 1, 1)));
     }
 }
